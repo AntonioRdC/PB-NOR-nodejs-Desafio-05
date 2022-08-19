6420e68e1ecf259ac53bc19a7d4e215fc30e5d93
Antonio Ribeiro <antonioribeiroamg@gmail.com>
2022-08-19 09:34:35 -0400

    feat: Create Route Patch

--- src/app/validations/product/updatePatch.ts
+++ src/app/validations/product/updatePatch.ts
@@ -4,12 +4,12 @@ import Joi from 'joi'
 export default async (req: Request, res: Response, next: NextFunction) => {
   try {
     const schema = Joi.object({
-      title: Joi.string().required().trim(),
-      description: Joi.string().required().trim(),
-      department: Joi.string().required().trim(),
-      brand: Joi.string().required().trim(),
-      price: Joi.number().required().min(0.01).max(1000),
-      qtd_stock: Joi.number().required().min(0).max(100000).integer(),
+      title: Joi.string().optional().trim(),
+      description: Joi.string().optional().trim(),
+      department: Joi.string().optional().trim(),
+      brand: Joi.string().optional().trim(),
+      price: Joi.number().optional().min(0.01).max(1000),
+      qtd_stock: Joi.number().optional().min(0).max(100000).integer(),
       bar_codes: Joi.string().forbidden(),
       stock_control_enabled: Joi.boolean().forbidden(),
       createdAt: Joi.string().forbidden(),
--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -3,6 +3,7 @@ import ProductController from '../app/controller/ProductController'
 import createValidation from '../app/validations/product/create'
 import getValidation from '../app/validations/product/get'
 import updatePutValidation from '../app/validations/product/updatePut'
+import updatePatchValidation from '../app/validations/product/updatePatch'
 
 const router = Router()
 
@@ -10,5 +11,6 @@ router.post('/api/v1/product', createValidation, ProductController.create)
 router.get('/api/v1/product', getValidation, ProductController.get)
 router.get('/api/v1/product/:id', ProductController.getById)
 router.put('/api/v1/product/:id', updatePutValidation, ProductController.update)
+router.patch('/api/v1/product/:id', updatePatchValidation, ProductController.update)
 
 export default router
